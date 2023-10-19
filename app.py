f62e8eb1b86c1515dbf53ced5935939a394a2fd7
SalvLav <richardjthies95@gmail.com>
2023-10-18 19:50:07 -0500

    added artist video info to render template return statement

--- app.py
+++ app.py
@@ -23,6 +23,8 @@ def get_artist_info_route():
     # Spotify will currently return information for artist's Stage name, followers count, artist image,albums w/images, and top tracks w/images
     spotify_information = get_spotify_artist_info(artist)
 
+    artist_video = singer_video(artist)
+
     # Checking if all information on artist is  not found for both api calls, return no artist found message template, else render template with their information
     if spotify_information is None and returnUser == None: # If we get a None , we should display a no info found on our template.
          return render_template('artist.html', artistName= 'No info found.')
@@ -30,7 +32,7 @@ def get_artist_info_route():
         #  Here we will return all the info that we get to create our Bio
          return render_template('artist.html', artistName= returnUser[0], artistCountry = returnUser[1], artistCity = returnUser[2],
                                 artistGender = returnUser[3],  artistBirth = returnUser[4], artistMusic = returnUser[5],
-                                 spotify_information=spotify_information ) # render our data, and send it to the html file to display.\
+                                 spotify_information=spotify_information, artist_video=artist_video) # render our data, and send it to the html file to display.\
     
     # if returnUser == None:
     #     return render_template('artist.html', artistName='No artist found.')
