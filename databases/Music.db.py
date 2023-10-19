08c33291c9b259e44d8b51f54037919eea962f08
nq2623up <DakotaBruffett@gmail.com>
2023-10-18 19:34:23 -0500

    Flask

--- databases/Music.db.py
+++ databases/Music.db.py
@@ -1,5 +1,6 @@
 import sqlite3
 from database import db
+
 conn = sqlite3.connect('Music_data.sqlite')
 
 conn.execute('Create a table for the song the user decides to input (Song Release date,artest name,song title)')
@@ -8,6 +9,7 @@ conn.execute('insert year of Release', ("2015"))
 conn.execute('insert artest data', ("The Oh hellos"))
 conn.execute('insert the song data', ("Soldier, Poet, King"))
 conn.commit()
+conn.close()
 for row in conn.execute('select * From Music data'):
     print(row)
 
