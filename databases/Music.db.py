7a8c2c158f8fb99493df985b4e42ef6c8eefd69b
nq2623up <DakotaBruffett@gmail.com>
2023-10-23 17:50:26 -0500

    Flask

--- databases/Music.db.py
+++ databases/Music.db.py
@@ -1,5 +1,7 @@
 import sqlite3
 from database import db
+from flask import Flask, render_template, request
+app = Flask(__name__)
 
 conn = sqlite3.connect('Music_data.sqlite')
 
@@ -14,6 +16,14 @@ conn.close()
 for row in conn.execute('select * From Music data'):
     print(row)
 
+@app.route('/save',methods=['POST'])
+def save_data(data):
+    data = request.get_json()
+    print(data)
+    value = "{{Music.data}}"
+    name = "Music.db"
+    value = request.form.get ('Music.db')
+    return 'saved'
 
 def music_data(self):
     Music_Stored = db.Music_Stored('dataStored')
