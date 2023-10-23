2ad60e47291761ea95a8c40b4cd39485c8ff7b30
SalvLav <richardjthies95@gmail.com>
2023-10-23 15:30:39 -0500

    Removed duplicate variables, they were already in the loop

--- youtube_api.py
+++ youtube_api.py
@@ -1,6 +1,15 @@
 from googleapiclient.discovery import build
 import os
 from pprint import pprint
+from googleapiclient.errors import HttpError
+import logging
+
+
+logging.basicConfig(#set up logging settings
+    level=logging.DEBUG,
+    filename='youtube_api.log',
+    filemode='a'
+)
 
 api_key = os.environ['YOUTUBE_API_KEY']
 api_name = 'youtube'
@@ -21,17 +30,13 @@ def singer_video(artist):
         response = request.execute()#response will be the json turned into a python dictionary?
         
         # pprint(response)#FOR (SOLO) TESTING
-        
-        title = response['items'][0]['snippet']['title']#pulling out the titles of the videos
-        
-        video_id = response['items'][0]['id']['videoId']#will unique identify videos
-        
+
         # print('---------------')#FOR (SOLO) TESTING
         five_video_list = []#create empty list to hold the five videos
 
         for item in response.get('items', []):#loop to return the value associated with the key 'items', which is a list of dictionaries. Otherwise returns empty list so it won't crash
-            title = item['snippet']['title']#drilling down to pull out the title and video id, and high thumbnail parameters 
-            video_id = item['id']['videoId']
+            title = item['snippet']['title']#pulling out the titles of the videos
+            video_id = item['id']['videoId']#will unique identify videos
             high_height = item['snippet']['thumbnails']['high']['height']#could choose betweeen default, high, or medium thumnails, which differ in size and img quality.
             high_url = item['snippet']['thumbnails']['high']['url']
             high_width = item['snippet']['thumbnails']['high']['width']
@@ -40,11 +45,11 @@ def singer_video(artist):
                                     'height': high_height, 
                                     'url': high_url, 
                                     'width': high_width})#package these items into dictionaries and add them all to the list 
-        # pprint(five_video_list)#FOR (SOLO) TESTING
+        pprint(five_video_list)#FOR (SOLO) TESTING
         return five_video_list#send back list of parameters to itentify videos
 
-    except:#refine try-except later
-        print('err')
+    except HttpError as e:
+        print(f'An error has occured: {e}')
 
 if __name__ == '__main__':#allows module to be run solo
     singer_video(input('Artist name? '))
