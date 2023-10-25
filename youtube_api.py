0ed9102464ccf7cb8dc05e1856f2337959470dae
SalvLav <richardjthies95@gmail.com>
2023-10-24 23:37:20 -0500

    Changed some variable names to fit current build, and removed paramters that are on longer being used

--- youtube_api.py
+++ youtube_api.py
@@ -32,11 +32,11 @@ def get_youtube_videos(artist):
             safeSearch='moderate'#string, could be moderate, strict, or none
         )#no .execute() here?
         response = request.execute()#response will be the json turned into a python dictionary?
-        # pprint(response)#FOR (SOLO) TESTING
-        # print('---------------')#FOR (SOLO) TESTING
-        five_video_list = extract_video_info(response)#send the json to function for parsing
+        pprint(response)#FOR (SOLO) TESTING
+        print('---------------')#FOR (SOLO) TESTING
+        twenty_video_list = extract_video_info(response)#send the json to function for parsing
         
-        return five_video_list#send back list of parameters to identify videos
+        return twenty_video_list#send back list of parameters to identify videos
 
     except HttpError as e:# Handles related HTTP errors such trying to access unauthorized resources, server errors, connetion problems and many more...
         logging.exception(e)
@@ -49,21 +49,17 @@ def get_youtube_videos(artist):
         print('An unexpected error has occured.')
 
 def extract_video_info(response):
-    five_video_list = []#create empty list to hold the five videos
+    twenty_video_list = []#create empty list to hold the twenty videos
 
     for item in response.get('items', []):#loop to return the value associated with the key 'items', which is a list of dictionaries. Otherwise returns empty list so it won't crash
         title = item['snippet']['title']#pulling out the titles of the videos
         video_id = item['id']['videoId']#will unique identify videos
-        high_height = item['snippet']['thumbnails']['high']['height']
         high_url = item['snippet']['thumbnails']['high']['url']#could choose betweeen default, high, or medium thumnails, which differ in size and img quality.
-        high_width = item['snippet']['thumbnails']['high']['width']
-        five_video_list.append({'title': title, 
+        twenty_video_list.append({'title': title, 
                                 'video_id': video_id, 
-                                'height': high_height, 
-                                'url': high_url, 
-                                'width': high_width})#package these items into dictionaries and add them all to the list 
-        pprint(five_video_list)#FOR (SOLO) TESTING
-    return five_video_list
+                                'url': high_url,})#package these items into dictionaries and add them all to the list 
+        pprint(twenty_video_list)#FOR (SOLO) TESTING
+    return twenty_video_list
 
 if __name__ == '__main__':#allows module to be run solo
     get_youtube_videos(input('Artist name? '))
